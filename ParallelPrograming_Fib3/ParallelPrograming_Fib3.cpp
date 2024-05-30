f7426edbaa285f3f46ca47162089b3ed29bbb4ed
Nightarc <yurik.1594@gmail.com>
2024-05-31 01:19:12 +0300

    lab3 done

--- ParallelPrograming_Fib3/ParallelPrograming_Fib3.cpp
+++ ParallelPrograming_Fib3/ParallelPrograming_Fib3.cpp
@@ -13,9 +13,10 @@ using namespace std;
 std::random_device rd; // obtain a random number from hardware
 std::mt19937 gen(rd()); // seed the generator
 std::uniform_int_distribution<> distr(1000, 2000); // define the range
+mutex mtx;
+int sharedVar = 0;
 
-
-const bool DEBUG = false;
+const bool DEBUG = true;
 template <typename TFunc> void RunAndMeasure(const char* title, TFunc func)
 {
     const auto start = std::chrono::steady_clock::now();
@@ -27,6 +28,20 @@ template <typename TFunc> void RunAndMeasure(const char* title, TFunc func)
 int nextRandom() {
     return distr(gen);
 }
+
+void task1() {
+    thread T1 = thread([]() { //thread that prints even numbers
+        for (size_t i = 0; i <= 100; i += 2)
+            cout << i << endl;
+        });
+    thread T2 = thread([]() { //thread that prints odd numbers
+        for (size_t i = 1; i <= 100; i += 2)
+            cout << i << endl;
+        });
+    
+    T1.join(); T2.join();
+}
+
 void foo()
 {
     std::cout << "Thread start..." << std::endl;
@@ -42,36 +57,20 @@ void foo()
 
 void task2(int N) {
     
+    vector<thread> threads;
     for (size_t i = 0; i < N; i++)
-    {
-        thread T = thread(foo);
-        T.detach(); 
-    }
-}
-int sharedVar = 0; //wrong, thread number should be limited
-void task3ThreadFunc(int i, std::mutex &mx) {
-    std::lock_guard<std::mutex> ulmx(mx);
-    if(sharedVar < 100) sharedVar += i;
-    cout << to_string(i) + " reporting: sharedVar: " + to_string(sharedVar) << endl;
-    if (sharedVar > 100) {
-        cout << "aborting!" << endl;
-        return;
-    }
-}
-void task3(int P) {
-    std::mutex mx;
+        threads.emplace_back(foo);
 
-    int i = 0;
-    while(sharedVar < 100)
-    {
-        thread T = thread(task3ThreadFunc, (i++), ref(mx));
-        T.detach();
+    for (auto& thread : threads) {
+        thread.join();
     }
 }
+
+
 bool s[1000000];
 int gpd(int n) {
     size_t gpd = 1;
-    for (size_t i = 1; i < sqrt(n); i++)
+    for (size_t i = 2; i <= sqrt(n); i++)
     {
         if (n % i == 0)
         {
@@ -99,28 +98,31 @@ vector<int> createVectorRandom(int n, int leftBorder, int rightBorder) {
 
     return res;
 }
-void task4_helper(vector<int> vec, int l, int r) {
+void task4_helper(vector<int>& vec, int l, int r) {
     for (size_t i = l; i < r; i++) {
         vec[i] = gpd(vec[i]);
-        if (DEBUG) cout << "vec[" + to_string(i) + "]: " + to_string(vec[i]) << endl;
+        if (DEBUG && vec.size() <= 100) cout << "vec[" + to_string(i) + "]: " + to_string(vec[i]) << endl;
     }
 }
 //if arr divides in even parts by threads, divide in even sectors and run threads
-int task4_run(int p, vector<int>& arr) {
-    if (arr.size() % p == 0) {
-        int partSize = arr.size() / p;
-        vector<thread> threads;
-        for (size_t i = 0; i < p; i++)
-            threads.push_back(thread(task4_helper, ref(arr), i*partSize, (i+1)*partSize));
+int task4_run(int p, vector<int> arr) {
+    int partSize = arr.size() / p;
+    vector<thread> threads;
+    for (size_t i = 0; i < p; i++)
+    {
+        int start = i * partSize;
+        int end = (i == p - 1) ? arr.size() : (i + 1) * partSize; // last thread fills all remaining space
+        threads.push_back(thread(task4_helper, ref(arr), start, end));
 
-        for (size_t i = 0; i < p; i++)
-            if(threads[i].joinable()) threads[i].join();
     }
+
+    for (auto& thread : threads) thread.join();
+
+    if (arr.size() < 100) printVector(arr);
     return 0;
 }   
 
-void task4(int N) {
-    
+chrono::milliseconds task4(int P, int N) {
     //Filling ertosphenes sieve
     for (size_t i = 0; i < 1000000; i++)
         s[i] = true;
@@ -129,7 +131,11 @@ void task4(int N) {
         for (size_t j = i*i; j < 1000000; j+=i)
             s[j] = false;
 
-    if (DEBUG) {
+    vector<int> arr = createVectorRandom(N, 1e5, 1e6);
+    return RunAndMeasure(to_string(N).c_str(), [P, &arr] {return task4_run(P, arr); }, true);
+    
+
+    /*if (DEBUG) {
         vector<int> arr = createVectorRandom(10, 10, 100);
         printVector(arr);
         RunAndMeasure("10", [N, &arr] {return task4_run(N, arr); });
@@ -141,28 +147,200 @@ void task4(int N) {
     else {
         vector<int> arr = createVectorRandom(2 * 1e5, (int)1e5, (int)1e6);
         RunAndMeasure("2*10e5", [N, &arr] {return task4_run(N, arr); });
+        arr.clear();
 
         arr = createVectorRandom(5 * 10e7, (int)1e5, (int)1e6);
         RunAndMeasure("5*10e7", [N, &arr] {return task4_run(N, arr); });
+        arr.clear();
 
         arr = createVectorRandom(5 * 10e8, (int)1e5, (int)1e6);
         RunAndMeasure("5*10e8", [N, &arr] {return task4_run(N, arr); });
+        arr.clear();
+    }*/
+
+
+}
+
+void increaseVar(int index) {
+    while (sharedVar < 100) {
+        this_thread::sleep_for(chrono::microseconds(1000)); //or else it tries to join thread in-creation
+        mtx.lock();
+        if (sharedVar < 100) sharedVar += index;
+        cout << index << ": sharedVar now is " << sharedVar << endl;
+        mtx.unlock();
+    }
+}
+
+//Задача 3: n потоков увеличивают общий ресурс - целочисленную переменную
+void task3() {
+    //Ввод P
+    cout << "Enter number of threads P: ";
+    int P; cin >> P;
+    if (P <= 0 || P > 10) {
+        std::cout << "Wrong thread count!" << std::endl;
+        return;
     }
 
+    vector<thread> threads; //вектор потоков
+    threads.reserve(P);
+    // Создание и запуск потоков
+    for (int i = 1; i <= P; ++i) {
+        threads.emplace_back(increaseVar, i);
+    }
 
+    //Ждём все потоки
+    for (auto& thread : threads)
+        thread.join();
+}
+
+//Задача 5 - последовательные и параллельный алгоритм вычисления произведения квадратной матрицы на вектор. Параллельный алгоритм учитывает доступное количество процессоров.
+//N = 500, 10000, 20000
+// Генерация квадратной матрицы размерности N
+std::vector<std::vector<double>> generateMatrix(int N) {
+    std::vector<std::vector<double>> matrix(N, std::vector<double>(N));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX; // Заполняем случайными значениями от 0 до 1
+        }
+    }
+    return matrix;
+}
+
+// Генерация вектора размерности N
+vector<double> generateVector(int N) {
+    std::vector<double> vector(N);
+    for (int i = 0; i < N; ++i) {
+        vector[i] = static_cast<double>(rand()) / RAND_MAX; 
+    }
+    return vector;
+}
+
+vector<double> parallelMatrixVectorMultiplication(const std::vector<std::vector<double>>& matrix, const std::vector<double>& vector) {
+    int N = matrix.size();
+    std::vector<double> result(N, 0.0);
+
+    for (int numThreads : {1, 2, 4, 8}) {
+        auto start = std::chrono::steady_clock::now();
+        std::vector<std::thread> threads(numThreads);
+        int chunkSize = N / numThreads;
+        for (int i = 0; i < numThreads; ++i) {
+            int start = i * chunkSize;
+            int end = (i == numThreads - 1) ? N : (i + 1) * chunkSize;
+            threads[i] = std::thread([&matrix, &vector, &result, &N, start, end]() {
+                for (int row = start; row < end; ++row) {
+                    for (int col = 0; col < N; ++col) {
+                        result[row] += matrix[row][col] * vector[col];
+                    }
+                }
+                });
+        }
+        for (auto& thread : threads) {
+            thread.join();
+        }
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> parallelDuration = end - start;
+        std::cout << "Параллельное время с " << numThreads << " ядрами: " << parallelDuration.count() << " секунд" << std::endl;
+    }
+
+    return result;
+}
+std::vector<double> MVMult_Seq(const std::vector<std::vector<double>>& matrix, const std::vector<double>& vector) {
+	int N = matrix.size();
+	std::vector<double> result(N);
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			result[i] += matrix[i][j] * vector[j];
+		}
+	}
+	return result;
+}
+void task5() {
+    std::vector<int> sizes = { 5000, 10000, 20000 };
+
+    for (int N : sizes) {
+        std::cout << "Matrix size: " << N << "x" << N << std::endl;
+
+        // Генерация матрицы и вектора
+        auto matrix = generateMatrix(N);
+        auto vector = generateVector(N);
+
+        // Замер времени для последовательного алгоритма
+        auto start = std::chrono::steady_clock::now();
+        auto seqResult = MVMult_Seq(matrix, vector);
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> seqDuration = end - start;
+        std::cout << "Sequential time: " << seqDuration.count() << " s" << std::endl;
+
+        auto parallelResult = parallelMatrixVectorMultiplication(matrix, vector);
+
+    }
+}
+
+//Сумма чисел от 1 до N
+void task6(int p, int N) {
+    auto start = std::chrono::steady_clock::now();
+    long long result = 0;
+    std::vector<std::thread> threads(p);
+    int chunkSize = N / p;
+    for (int i = 0; i < p; ++i) {
+        int start = i * chunkSize;
+        int end = (i == p - 1) ? N : (i + 1) * chunkSize;
+        threads[i] = std::thread([&result, &N, start, end]() {
+            long long localSum = 0;
+            for (size_t j = start; j < end; j++) 
+                localSum += j;
+
+            std::lock_guard<std::mutex> lock(mtx);
+            result += localSum;
+            });
+    }
+    for (auto& thread : threads) {
+        thread.join();
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> parallelDuration = end - start;
+    std::cout << "time " << p << " threads: " << parallelDuration.count() << " s" << std::endl;
 }
 int main()
 {
+    int state = -1;
     int N;
-    cout << "Input N: "; cin >> N;
+    int p[] = { 1, 2, 4, 8 };
+    int ns[] = { 2 * 1e7, 5 * 1e7, 1e8 };
+    cout << "Input task number: "; 
+    cin >> state;
+    switch (state) {
+        case 1:
+            task1();
+            break;
+        case 2:
+            cout << "Input N <= 10: "; cin >> N;
+            task2(N);
+            break;
+        case 3:
+            task3();
+            break;
+        case 4:
+        {
+            for (size_t i = 0; i < 4; i++)
+            {
+                chrono::milliseconds time = (chrono::milliseconds)0;
+                for (size_t i = 0; i < 3; i++)
+                {
+                    auto start = std::chrono::steady_clock::now();
+                    task4(p[i], ns[i]);
+                    auto end = std::chrono::steady_clock::now();
+                    time += chrono::duration<double>(end - start).count();
+                }
+            }
+            break;
+        }
+        case 5:
+            break;
+        case 6:
+            break;
+    }
     
-    task4(N);
     return 0;
 }
 
-/*
-* 1.01284	0.1558	0.5249
-6.44357	2.2128	10.4005
-67.39	38.8445	83.2629
-
-*/
