b88e3c41bd43f3f477a3240ef94dc170dc954856
Nightarc <yurik.1594@gmail.com>
2024-06-03 10:19:41 +0300

    lab6 setup

--- ParallelPrograming_Fib3/main.cpp
+++ ParallelPrograming_Fib3/main.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
+#include <mpi.h>
+
 using namespace std;
-
-int main() {
-
+int main(int argc, char** argv)
+{
+	MPI_Init(&argc, &argv);
+	std::cout << "Hello World!\n";
+	MPI_Finalize();
 }
